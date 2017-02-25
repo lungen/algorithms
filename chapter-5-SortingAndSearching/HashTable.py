0d6f0121270a8d2cd03f176521c40ea02e632496
lungen <lungenfetzer@gmail.com>
2017-02-26 00:47:22 +0100

    fine

---
+++ chapter-5-SortingAndSearching/HashTable.py
@@ -0,0 +1,71 @@
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        hash_value = self.hash_function(key, len(self.slots))
+
+        if self.slots[hash_value] == None:
+            self.slots[hash_value] = key
+            self.data[hash_value] = data
+        else:
+            if self.slots[hash_value] == key:
+                self.data[hash_value] = data  # replace
+            else:
+                next_value = self.rehash(hash_value, len(self.slots))
+                while self.slots[next_value] != None \
+                      and self.slots[next_value] != key:
+                    next_value = self.rehash(next_value, len(self.slots))
+
+                if self.slots[next_value] == None:
+                    self.slots[next_value] = key
+                    self.data[next_value] = data
+                else:
+                    self.data[next_value] = data
+
+    def hash_function(self, key, size):
+        return key % size
+
+    def rehash(self, old_hash, size):
+        return (old_hash + 1) % size
+
+    def get(self, key):
+        start_slot = self.hash_function(key, len(self.slots))
+
+        position = start_slot
+        found = False
+        stop = False
+        data = None
+        while self.slots[position] != None \
+              and not found and not stop:
+
+            if self.slots[position] == key:
+                found == True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == start_slot:
+                    stop = True
+
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        return self.put(key, data)
+
+H = HashTable()
+H[54] = 'cat'
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+H[20]="chicken"
+print(H.slots)
+print(H.data)
